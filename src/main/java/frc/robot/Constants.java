09a74ab78f666261a83e38b0d1847f5c60bfd337
amarrri-69 <98570914+amarrri-69@users.noreply.github.com>
2022-05-30 16:23:50 -0400

    constants for indexer

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,11 @@ public class Constants {
         public static final double kDistPerRot = 3.072 / 100;
     }
 
+    public static final class Indexer {
+      public static final int kPort = 0;
+      public static final double kOneBallTick = 0.69;
+    }
+
     public static final class Drivetrain {
         public static final double kThrottleFilter = 1.5;
         public static final double kTurnFilter = 1.5;
